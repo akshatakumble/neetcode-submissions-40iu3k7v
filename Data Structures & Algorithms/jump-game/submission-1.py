6305d470239343cb99a197975ee0f85d1bbc1939
Akshata Kumble <55057698+akshatakumble@users.noreply.github.com>
2026-04-14 05:01:47 +0530

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        memo = {}
+        def dfs(i):
+            if i == len(nums) - 1:
+                return True
+            if i in memo:
+                return memo[i]
+            end = min(len(nums) - 1, i + nums[i])
+            for j in range(end, i, -1):
+                if dfs(j):
+                    memo[i] = True
+                    return True
+            memo[i] = False
+            return False
+
+        return dfs(0)
