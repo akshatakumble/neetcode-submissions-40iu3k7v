10b4b4614542341e0cdd8df844dadd5a61061eee
Akshata Kumble <55057698+akshatakumble@users.noreply.github.com>
2026-04-16 05:17:17 +0530

    Remove Legacy Machine Learning/ folder

--- Machine Learning/gradient-descent/submission-0.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def get_minimizer(self, iterations: int, learning_rate: float, init: int) -> float:
-        for i in range(iterations):
-            d = 2 * init #(derivative of x sq) and init is curr_guess
-            init = init - d * learning_rate
-        return round(init,5)
